3966b62605fb1c45552d091367d52091ff4a657b
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-10-17 12:41:46 -0400

    commit tree dff9ca308cef9d3f68b58bde4ad16662d41f6b0d

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1=2;
+const num2=31;
+const multiply= (num1*num2);
+
+const random =Math.floor(Math.random()* 2) + 1;
+
+const num3=16;
+const num4=6;
+const mod=(num3 % num4);//long division
+
+const max=(1,2,3,7,18,19,20);
